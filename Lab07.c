61152f16211ab1aeaaf58033b4848fd00425d1ad
Prof. John Hutton <110941287+jfhutton@users.noreply.github.com>
2022-10-21 12:34:22 -0500

    Initial checkin.

---
+++ Lab07.c
@@ -0,0 +1,30 @@
+/***************************************************************************
+ * Lab07.c (barebones starter code version)
+ *
+ *   Created:
+ *    Author:
+ * Processor: ATmega128A (on a ReadyAVR)
+ *
+ * Your comments go here...
+ *
+ **************************************************************************/
+
+/* your #include file declaration(s) go here */
+
+/* your #define declarations go here */
+#define BaudRate 9600
+#define DataBits 8
+#define StopBits 1
+#define NoParity 0
+
+// prototype subroutine definitions
+void initializeArray(unsigned char *cp);
+void displayArray(unsigned char *cp);
+void updateArray(unsigned char *cp);
+
+int main(void)
+{
+    /* your code goes here */
+}
+
+/* your subroutines go here */
---
+++ README.md
@@ -0,0 +1,4 @@
+# Lab07
+In this lab you will get practice using C pointers. Specifically, you will write a program that implements a 1-D, 122-generation cellular automata (CA) simulation, sending each generation out the USART to be displayed on a PC-based communications terminal. To perform the USART communication, you’ll use subroutines from a UART library provided by your instructor, and you will make use of C pointers to read and write elements in the CA data array. 
+
+Have fun!
