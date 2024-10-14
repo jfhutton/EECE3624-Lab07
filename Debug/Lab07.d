31902188702174d27952df2c28b6db73e086de4e
Prof. John Hutton <110941287+jfhutton@users.noreply.github.com>
2024-10-14 16:15:09 -0500

    Tweaked starter code
    Added a few more placeholders and a while "Hello world!" loop to make confirming the UART is working easy.

---
+++ Debug/Lab07.d
@@ -0,0 +1,3 @@
+Lab07.d Lab07.o: .././Lab07.c .././UARTLibrary.h
+
+.././UARTLibrary.h:
--- Lab07.c
+++ Lab07.c
@@ -10,6 +10,7 @@
  **************************************************************************/
 
 /* your #include file declaration(s) go here */
+#include "UARTLibrary.h"
 
 /* your #define declarations go here */
 #define BaudRate 9600
@@ -24,7 +25,33 @@ void updateArray(unsigned char *cp);
 
 int main(void)
 {
+	// create a 61-char array
+
+
+	// Initialize the UART
+	// ProfNote:  Understand the UARTLibary.h before blindly running the next line!
+	uart_init(BaudRate, DataBits, StopBits, NoParity);
+	
+	// ProfNote:  Initial Testing Only!  Comment out after UART confirmed working!
+	while (1) {
+		uart_tx('H');uart_tx('e');uart_tx('l');uart_tx('l');uart_tx('o');uart_tx('\r');uart_tx('\n');
+		uart_tx('W');uart_tx('o');uart_tx('r');uart_tx('l');uart_tx('d');uart_tx('!');uart_tx('\r');uart_tx('\n');
+	}
+	
     /* your code goes here */
+
 }
 
 /* your subroutines go here */
+void initializeArray(unsigned char *cp){
+	
+}
+void displayArray(unsigned char *cp){
+	// ProfNote:  Look through your line using "uart_tx(<char>)" to print one character at a time.
+	// At the end of the line, use "uart_tx('\r'); uart_tx('\n');" to print a new line to keep your
+	// output aligned properly.
+	
+}
+void updateArray(unsigned char *cp){
+	
+}
